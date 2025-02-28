58c441f59c86e9408f99e8bddc347cbd60831f04
Alex0 <alexisguaza07@gmail.com>
2025-02-27 21:49:13 -0500

    Initial commit PROYECT

---
+++ src/test/java/despacho/proveedor/provedor/ProvedorApplicationTests.java
@@ -0,0 +1,13 @@
+package despacho.proveedor.provedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
