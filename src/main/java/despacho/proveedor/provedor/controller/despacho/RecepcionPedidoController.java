c8c8c4c8069bfaa7d808395b790829e2e0043491
Alex0 <alexisguaza07@gmail.com>
2025-03-01 21:40:11 -0500

    funcionando, revisar que se va a entregar

--- src/main/java/despacho/proveedor/provedor/controller/despacho/RecepcionPedidoController.java
+++ src/main/java/despacho/proveedor/provedor/controller/despacho/RecepcionPedidoController.java
@@ -3,12 +3,16 @@ package despacho.proveedor.provedor.controller.despacho;
 import despacho.proveedor.provedor.Dto.despacho.RecepcionPedidoDTO;
 import despacho.proveedor.provedor.model.despacho.RecepcionPedido;
 import despacho.proveedor.provedor.service.despacho.RecepcionPedidoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/despacho/recepcion")
+@Tag(name = "Recepción de Pedidos", description = "API para gestionar la recepción de pedidos de proveedores")
+
 public class RecepcionPedidoController {
 
     private final RecepcionPedidoService service;
