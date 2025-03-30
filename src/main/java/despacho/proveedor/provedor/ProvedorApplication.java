1c5e3ca7607064fdde212a276c55a3e9b1d772ea
Daniel Valencia <stickblack14@gmail.com>
2025-03-30 11:58:41 -0500

    Agregando funcionalidades solicitadas a la rama D_Estiwer

--- src/main/java/despacho/proveedor/provedor/ProvedorApplication.java
+++ src/main/java/despacho/proveedor/provedor/ProvedorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProvedorApplication {
+public class 4ProvedorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProvedorApplication.class, args);
